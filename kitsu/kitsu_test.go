31527148797c6ba4910c8a9117172adac6d7581a
Stratos Neiros <nstratos@gmail.com>
2016-12-27 12:51:09 +0200

    Add parse error test for bad URL case

--- kitsu/kitsu_test.go
+++ kitsu/kitsu_test.go
@@ -1,6 +1,9 @@
 package kitsu
 
-import "testing"
+import (
+	"net/url"
+	"testing"
+)
 
 func TestNewClient(t *testing.T) {
 	c := NewClient(nil)
@@ -10,23 +13,26 @@ func TestNewClient(t *testing.T) {
 	}
 }
 
-func TestNewRequest(t *testing.T) {
+func TestClient_NewRequest(t *testing.T) {
 	c := NewClient(nil)
 
 	inURL, outURL := "/foo", defaultBaseURL+"foo"
 	req, _ := c.NewRequest("GET", inURL, nil)
 
-	// Test that the base URL is added to the endpoint.
+	// Test that the client's base URL is added to the endpoint.
 	if got, want := req.URL.String(), outURL; got != want {
 		t.Errorf("NewRequest(%q) URL is %q, want %q", inURL, got, want)
 	}
 }
 
-func TestClient_NewRequest_badEndpoint(t *testing.T) {
+func TestClient_NewRequest_badURL(t *testing.T) {
 	c := NewClient(nil)
-	inURL := "%foo"
+	inURL := ":"
 	_, err := c.NewRequest("GET", inURL, nil)
 	if err == nil {
 		t.Errorf("NewRequest(%q) should return parse err", inURL)
+	}
+	if err, ok := err.(*url.Error); !ok || err.Op != "parse" {
+		t.Errorf("Expected URL parse error, got %+v", err)
 	}
 }
