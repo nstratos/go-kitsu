aa1cdfb53d8e422e53b8aa001af20587f3008291
Stratos Neiros <nstratos@gmail.com>
2016-12-27 15:30:31 +0200

    Make NewRequest encode body as JSON

--- kitsu/kitsu.go
+++ kitsu/kitsu.go
@@ -1,6 +1,9 @@
 package kitsu
 
 import (
+	"bytes"
+	"encoding/json"
+	"io"
 	"net/http"
 	"net/url"
 )
@@ -40,18 +43,32 @@ type service struct {
 
 // NewRequest creates an API request. If a relative URL is provided in urlStr,
 // it will be resolved relative to the BaseURL of the Client. Relative URLs
-// should always be specified without a preceding slash.
+// should always be specified without a preceding slash. If body is specified,
+// it will be encoded to JSON and used as the request body.
 func (c *Client) NewRequest(method, urlStr string, body interface{}) (*http.Request, error) {
 	rel, err := url.Parse(urlStr)
 	if err != nil {
 		return nil, err
 	}
 
+	var buf io.ReadWriter
+	if body != nil {
+		buf = new(bytes.Buffer)
+		err := json.NewEncoder(buf).Encode(body)
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	u := c.BaseURL.ResolveReference(rel)
 
-	req, err := http.NewRequest(method, u.String(), nil)
+	req, err := http.NewRequest(method, u.String(), buf)
 	if err != nil {
 		return nil, err
+	}
+
+	if body != nil {
+		req.Header.Set("Content-type", defaultMediaType)
 	}
 
 	return req, nil
--- kitsu/kitsu_test.go
+++ kitsu/kitsu_test.go
@@ -1,6 +1,7 @@
 package kitsu
 
 import (
+	"io/ioutil"
 	"net/url"
 	"testing"
 )
@@ -17,11 +18,23 @@ func TestClient_NewRequest(t *testing.T) {
 	c := NewClient(nil)
 
 	inURL, outURL := "/foo", defaultBaseURL+"foo"
-	req, _ := c.NewRequest("GET", inURL, nil)
+	inBody, outBody := &struct{ Foo string }{Foo: "bar"}, `{"Foo":"bar"}`+"\n"
+	req, _ := c.NewRequest("GET", inURL, inBody)
 
 	// Test that the client's base URL is added to the endpoint.
 	if got, want := req.URL.String(), outURL; got != want {
 		t.Errorf("NewRequest(%q) URL is %q, want %q", inURL, got, want)
+	}
+
+	// Test that body gets encoded to JSON.
+	body, _ := ioutil.ReadAll(req.Body)
+	if got, want := string(body), outBody; got != want {
+		t.Errorf("NewRequest(%#v) Body is \n%q, want \n%q", inBody, got, want)
+	}
+
+	// Test that the correct Content-Type gets added.
+	if got, want := req.Header.Get("Content-Type"), defaultMediaType; got != want {
+		t.Errorf("NewRequest() Content-Type is %q, want %q", got, want)
 	}
 }
 
